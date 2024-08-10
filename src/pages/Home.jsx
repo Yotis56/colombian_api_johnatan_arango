ab52dbbdf7b30397e22d9e9d91bd19afe8418fb2
@Yotis56 <ingyotis@gmail.com>
2024-08-10 18:14:58 -0500

    retrieved information from the API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react"
+
+const API = 'https://api-colombia.com/api/v1/'
 
 const Home = ( ) => {
+    useEffect( () => {
+        const getData = async (endpoint) => {
+            try {
+                const rawData = await fetch(`${API}${endpoint}`)
+                const data = await rawData.json()
+                switch(endpoint){
+                    case 'President':
+                        setPresidentes(data)
+                        break
+                    case 'TouristicAttraction':
+                        setAtracciones(data)
+                        break
+                    case 'Airport':
+                        setAeropuertos(data)
+                        break
+                }
+            } catch (e) {
+                console.error(e)
+            }
+        }
+        getData('President')
+        getData('TouristicAttraction')
+        getData('Airport')
+
+    }, [])
+
+    const [presidentes, setPresidentes] = useState([])
+    const [aeropuertos, setAeropuertos] = useState([])
+    const [atracciones, setAtracciones] = useState([])
     return (
         <>
             <p>Home page under construction</p>
