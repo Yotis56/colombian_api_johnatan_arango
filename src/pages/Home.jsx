42bde962d54bb4cf02060db3a09ae5fbe4bd3e0a
@Yotis56 <ingyotis@gmail.com>
2024-08-10 23:44:26 -0500

    functions to group Airports and Atractions added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,12 @@ import { useEffect, useState } from "react"
 
 const API = 'https://api-colombia.com/api/v1/'
 
-const partidosPolEjemplo = [
-    {
-        name: 'Partido Liberal',
-        noPresidents: 2, 
-        presidents: [
-            // {...},
-            // {...},
-        ]
-    },
-    /* { Se repite la estructura con otro objeto de un partido político}*/ 
-]
 const Home = ( ) => {
+    const [presidentes, setPresidentes] = useState([])
+    const [aeropuertos, setAeropuertos] = useState([])
+    const [atracciones, setAtracciones] = useState([])
+    const [departamentos, setDepartamentos] = useState([])
+
     useEffect( () => {
         const getData = async (endpoint) => {
             try {
@@ -26,43 +20,118 @@ const Home = ( ) => {
                     case 'TouristicAttraction':
                         setAtracciones(data)
                         break
-                        case 'Airport':
-                            setAeropuertos(data)
-                            break
-                        }
-                    } catch (e) {
-                        console.error(e)
-                    }
+                    case 'Airport':
+                        setAeropuertos(data)
+                        break
+                    case 'Department':
+                        setDepartamentos(data)
+                        break
+                }
+            } catch (e) {
+                console.error(e)
+            }
         }
         getData('President')
         getData('TouristicAttraction')
         getData('Airport')
+        getData('Department')
         
     }, [])
     
-    const [presidentes, setPresidentes] = useState([])
-    const [aeropuertos, setAeropuertos] = useState([])
-    const [atracciones, setAtracciones] = useState([])
-    let partidosPol = []
+    let partidosGrouped = []
+    let atraccionesGrouped = []
+    let aeropuertosGrouped = []
 
-    const orderPresidents = () => {
+    const groupPresidents = () => {
         presidentes.forEach( presidente => {
-            const partido = partidosPol.find( partido => partido.name.toLowerCase() === presidente.politicalParty.toLowerCase())
-            if (partido) {
-                const index = partidosPol.findIndex( partido => partido.name.toLowerCase() === presidente.politicalParty.toLowerCase())
-                partidosPol[index].noPresidents += 1
-                partidosPol[index].presidents.push(presidente)
+            const existsPartido = partidosGrouped.find( partido => partido.name.toLowerCase() === presidente.politicalParty.toLowerCase())
+            if (existsPartido) {
+                const index = partidosGrouped.findIndex( partido => partido.name.toLowerCase() === presidente.politicalParty.toLowerCase())
+                partidosGrouped[index].count += 1
+                partidosGrouped[index].presidents.push(presidente)
             } else {
-                partidosPol.push({
+                partidosGrouped.push({
                 name: presidente.politicalParty,
-                noPresidents: 1,
+                count: 1,
                 presidents: [presidente]})               
             }
         })
-        partidosPol.sort((a,b) => b.noPresidents - a.noPresidents)
+        partidosGrouped.sort((a,b) => b.count - a.count)
     }
-    orderPresidents()
-    console.log(partidosPol)
+    
+    const groupAtractions = () => {
+        atracciones.forEach( atraccion => {
+            // obtengo el objeto Departamento de la atracción
+            const departmentAtraction = departamentos.find( departamento => departamento.id === atraccion.city.departmentId )
+            // miro si ese departamento ya está en mi array agrupado
+            const departmentIndex = atraccionesGrouped.findIndex( item => item.id === departmentAtraction.id)
+            if (departmentIndex !== -1) {
+                // Si el departamento existe, ahora busco si existe la ciudad en el array agrupado
+                const cityIndex = atraccionesGrouped[departmentIndex].cities.findIndex( item => item.id === atraccion.cityId)
+                if (cityIndex !== -1){
+                    // si el departamento y la ciudad existen, simplemente aumento la cuenta
+                    atraccionesGrouped[departmentIndex].count += 1
+                    atraccionesGrouped[departmentIndex].cities[cityIndex].count += 1
+                } else {
+                    // en este caso, existe el departamento, pero tengo que crear la ciudad.
+                    atraccionesGrouped[departmentIndex].cities.push({
+                        city: atraccion.city.name,
+                        id: atraccion.cityId,
+                        count: 1,
+                    })
+                    atraccionesGrouped[departmentIndex].count += 1
+                }
+            } else {
+                // en este caso, no existe ni el departamento ni la ciudad en el array agrupado, así que creo ambos.
+                atraccionesGrouped.push({
+                    departamento: departmentAtraction.name,
+                    id: departmentAtraction.id,
+                    count: 1, 
+                    cities: [{
+                        city: atraccion.city.name,
+                        id: atraccion.cityId,
+                        count: 1
+                    }]
+                })
+            }
+        })
+    }
+
+
+    const groupAirports = () => {
+        aeropuertos.forEach( aeropuerto => {
+            const departmentIndex = aeropuertosGrouped.findIndex( item => item.id === aeropuerto.deparmentId)
+            if (departmentIndex !== -1){
+                aeropuertosGrouped[departmentIndex].count += 1
+                const cityIndex = aeropuertosGrouped[departmentIndex].cities.findIndex( item => item.id === aeropuerto.cityId)
+                if (cityIndex !== -1) {
+                    aeropuertosGrouped[departmentIndex].cities[cityIndex].count += 1
+                } else {
+                    aeropuertosGrouped[departmentIndex].cities.push({
+                        city: aeropuerto.city.name,
+                        id: aeropuerto.cityId,
+                        count: 1
+                    })
+                }
+            } else {
+                aeropuertosGrouped.push({
+                    department: aeropuerto.department.name,
+                    id: aeropuerto.deparmentId,
+                    count: 1,
+                    cities: [{
+                        city: aeropuerto.city.name,
+                        id: aeropuerto.cityId,
+                        count: 1
+                    }]
+                })
+            }
+        })
+    }
+
+    groupPresidents()
+    groupAtractions()
+    groupAirports()
+    console.log(aeropuertosGrouped)
     return (
         <>
             <p>Home page under construction</p>
